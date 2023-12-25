99c8bd6c84da0ef52d0a382476b774c43788a5b4
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-25 23:28:21 +0300

    Fix minor issue with node modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
     theme: {
     extend: {
       colors:{
-        primary: "hsl(47, 88%, 63%)"
+        primary:"hsl(47, 88%, 63%)"
       },
       fontFamily:{
         figtree: "Figtree"
