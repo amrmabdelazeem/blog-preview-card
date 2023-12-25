e92b20b096dfa03fd7328d7e3e5d6d0e7207ce02
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-25 22:10:36 +0300

    Add required font and set primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
     theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: "hsl(47, 88%, 63%)"
+      },
+      fontFamily:{
+        figtree: "Figtree"
+      }
+    },
   },
   plugins: [],
 }
